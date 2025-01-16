1b663f636150187ca6f0cc30c0dfc981508a5085
airnesygroup <airnesy.info@gmail.com>
2025-01-16 15:29:49 +0300

    new   commit

--- client/src/components/FeaturedPosts.jsx
+++ client/src/components/FeaturedPosts.jsx
@@ -34,8 +34,8 @@ const FeaturedPosts = () => {
           <div style={{ height: "60vh" }}  className="flex flex-col lg:flex-row gap-[100px] mt-4">
 
       {/* Left Section */}
-      <div className="lg:w-1.5/3 flex flex-col items-start gap-4 p-4  rounded-md">
-        <h1 className="text-2xl lg:text-6xl font-bold text-[var(--textColor)]">
+      <div className="lg:w-1/3 flex flex-col items-start gap-4 p-4  rounded-md">
+        <h1 className="text-2xl lg:text-4xl font-bold text-[var(--textColor)]">
           #1 most downloaded <span className="text-blue-600">book summary</span> app
         </h1>
         <p className="text-[var(--textColor)]">
@@ -48,7 +48,7 @@ const FeaturedPosts = () => {
           Get Started
         </Link>
       </div>
-      <div className="lg:w-0.5/3 flex flex-col items-start gap-4 p-4  rounded-md">
+      <div className="lg:w-1/3 flex flex-col items-start gap-4 p-4  rounded-md">
 </div>
 
       {/* Right Section */}
