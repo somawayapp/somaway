5560ee44902908ec323c4ba9a3aafd3900eff31a
airnesygroup <airnesy.info@gmail.com>
2025-01-16 15:42:58 +0300

    new   commit

--- client/src/components/FeaturedPosts.jsx
+++ client/src/components/FeaturedPosts.jsx
@@ -34,7 +34,7 @@ const FeaturedPosts = () => {
           <div style={{ height: "60vh" }}  className="flex flex-col lg:flex-row gap-[100px] mt-4">
 
       {/* Left Section */}
-      <div className="lg:w-1/3 flex flex-col items-start gap-4 p-4  rounded-md">
+      <div className="lg:w-1.1/3 flex flex-col items-start gap-4 p-4  rounded-md">
         <h1 className="text-2xl lg:text-6xl font-bold text-[var(--textColor)]">
           #1 most downloaded <span className="text-blue-600">book summary</span> app
         </h1>
@@ -49,27 +49,33 @@ const FeaturedPosts = () => {
         </Link>
       </div>
     
-      <div className="lg:w-0.5/3 flex flex-col items-start gap-4 p-4  rounded-md">
+      <div className="lg:w-0.9/3 flex flex-col items-start gap-4 p-4  rounded-md">
       </div>
 
+
       {/* Right Section */}
-      <div className="lg:w-1.5/3 flex flex-col gap-6">
+      <div className="lg:w-1/3 flex flex-col gap-6">
         {/* First Featured Post */}
         <div className="flex flex-col gap-1">
-          <Link to={`/${posts[0].slug}`} className="relative w-full" style={{ paddingTop: '5%', height: "60vh" }}>
+          <Link to={`/${posts[0].slug}`} className="relative w-full" style={{ paddingTop: '50%', height: "60vh" }}>
             <Image
               src={posts[0].img}
               className="absolute top-0 left-0 w-full object-cover rounded-md"
             />
           </Link>
           <div className="">
-        
+            <Link
+              to={`/posts?category=${posts[0].category}`}
+              className="text-blue-600 text-sm font-semibold uppercase"
+            >
+              {posts[0].category}
+            </Link>
           
           </div>
         </div>
 
       </div>
-     
+      
 
       </div>
 
