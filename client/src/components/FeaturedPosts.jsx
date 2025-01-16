72aa7778edc2d1f005755d2f5dc5d5c296889244
airnesygroup <airnesy.info@gmail.com>
2025-01-16 18:55:39 +0300

    new   commit

--- client/src/components/FeaturedPosts.jsx
+++ client/src/components/FeaturedPosts.jsx
@@ -92,45 +92,26 @@ const FeaturedPosts = () => {
           <p className=" text-lg md:text-xl text-[var(--textColor)]0">For you</p>
         </div>
 
-        {/* Additional Featured Posts */}
-        <div className="scroll-container">
-  <div className="scroll-content">
-    {posts.slice(1, 8).map((post, index) => (
-      <div key={index} className="scroll-item flex flex-col gap-2 w-full sm:w-1/2 md:w-1/4 lg:w-1/7">
-        <Link to={`/${post.slug}`} className="relative w-full" style={{ paddingTop: '150%' }}>
-          <Image
-            src={post.img}
-            className="absolute top-0 left-0 w-full h-full object-cover rounded-md"
-          />
-        </Link>
-        <Link
-          to={`/posts?category=${post.category}`}
-          className="text-[var(--textColor)] text-xs font-semibold"
-        >
-          {post.category}
-        </Link>
-      </div>
-    ))}
-    {/* Duplicate the content to create the infinite loop effect */}
-    {posts.slice(1, 8).map((post, index) => (
-      <div key={`duplicate-${index}`} className="scroll-item flex flex-col gap-2 w-full sm:w-1/2 md:w-1/4 lg:w-1/7">
-        <Link to={`/${post.slug}`} className="relative w-full" style={{ paddingTop: '150%' }}>
-          <Image
-            src={post.img}
-            className="absolute top-0 left-0 w-full h-full object-cover rounded-md"
-          />
-        </Link>
-        <Link
-          to={`/posts?category=${post.category}`}
-          className="text-[var(--textColor)] text-xs font-semibold"
-        >
-          {post.category}
-        </Link>
-      </div>
-    ))}
-  </div>
-</div>
 
+        {/* Additional Featured Posts */}
+        <div className="grid grid-cols-3 sm:grid-cols-7 gap-4">
+          {posts.slice(1, 8).map((post, index) => (
+            <div key={index} className="flex flex-col gap-2">
+              <Link to={`/${post.slug}`} className="relative w-full" style={{ paddingTop: '150%' }}>
+                <Image
+                  src={post.img}
+                  className="absolute top-0 left-0 w-full h-full object-cover rounded-md"
+                />
+              </Link>
+              <Link
+                to={`/posts?category=${post.category}`}
+                className="text-[var(--textColor)] text-xs font-semibold "
+              >
+                {post.category}
+              </Link>
+            </div>
+          ))}
+        </div>
     </div>
   );
 };
