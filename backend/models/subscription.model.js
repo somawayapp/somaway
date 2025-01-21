73de04256805510457d97aac6ffc3ccef5e0ccb3
airnesygroup <airnesy.info@gmail.com>
2025-01-21 15:10:05 +0300

    new   commit

---
+++ backend/models/subscription.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const subscriptionSchema = new Schema(
+  {
+    plan: {
+      type: String,
+      enum: ['monthly', 'annual', 'none'],
+      default: 'none',
+    },
+    startDate: {
+      type: Date,
+    },
+    endDate: {
+      type: Date,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'inactive',
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Subscription", subscriptionSchema);
