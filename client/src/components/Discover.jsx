875d88e55e9dc5e3d862238344c6efe120c8326d
airnesygroup <airnesy.info@gmail.com>
2025-01-24 18:12:58 +0300

    new   commit

--- client/src/components/Discover.jsx
+++ client/src/components/Discover.jsx
@@ -10,7 +10,7 @@ const fetchPosts = async (pageParam, searchParams) => {
   const searchParamsObj = Object.fromEntries([...searchParams]);
 
   const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`, {
-    params: { page: pageParam, ...searchParamsObj }, // Changed limit to 30
+    params: { page: pageParam, limit: 50000, ...searchParamsObj }, 
   });
   return res.data;
 };
