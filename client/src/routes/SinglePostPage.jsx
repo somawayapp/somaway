37fcdf795e2b2286bdd7cf401c121e2dd9b2c2f6
airnesygroup <airnesy.info@gmail.com>
2025-01-27 00:12:31 +0300

    new   commit

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -29,8 +29,8 @@ const SinglePostPage = () => {
     <div>
       <Navbar />
 
-      <div className="flex flex-col  p-2 md:p-8 mt-[10px] b-t-1 b-t-[var(--textColore)] gap-4">
-        <div className="flex flex-col md:flex-row gap-4 md:gap-8">
+      <div className="flex flex-col p-2 md:p-8 mt-[10px] border-t border-t-[var(--textColor)] gap-4">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-8">
           {data.img && (
             <div className="w-full md:w-1/4 flex justify-center md:block">
              <Image
