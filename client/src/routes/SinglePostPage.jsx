e5ea4599a0ecbbbc0fda1e4e0cdda20123745a57
airnesygroup <airnesy.info@gmail.com>
2025-01-26 23:14:46 +0300

    new   commit

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -1,14 +1,9 @@
-
-
-
 import { Link, useParams } from "react-router-dom";
 import Image from "../components/Image";
 import PostMenuActions from "../components/PostMenuActions";
 import Comments from "../components/Comments";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-import { format } from "timeago.js";
-import MainCategories from "../components/MainCategories";
 import Navbar from "../components/Navbar";
 import { FaStar } from "react-icons/fa";
 
@@ -31,98 +26,44 @@ const SinglePostPage = () => {
 
   return (
     <div>
-     <Navbar/>
+      <Navbar />
 
-    <div className="flex flex-col shadow-2xl p-[1px] md:p-8 mt-[10px] gap-4">
-        
-
-      <div className="flex gap-4 md:gap-8">
-
-      {data.img && (
-          <div className=" w-1/4">
-            <Image src={data.img} w="400" className="rounded-2xl" />
-          </div>
-        )}
-
-        <div className="lg:w-2/4 flex flex-col gap-2">
-        <p className="  text-[var(--softTextColor)]  ">
-          SUMMARY OF
-        </p>
-          <h1 className="text-xl md:text-4xl font-semibold">
-            {data.title}
-          </h1>
-          <div className="flex items-center gap-2 text-[var(--textColor)] text-md md:text-lg ">
-            <span>Book by</span>
-            <Link >{data.user.username}</Link>
-            <span>Category</span>
-            <Link>{data.category}</Link>
-          </div>
-           <div className="flex flex-row">
-               <FaStar  className="text-orange-500 w-[40px] text-orange-500 ml-[-5px] "   />
-               <FaStar  className="text-orange-500 w-[40px] text-orange-500  ml-[-5px]"  />
-               <FaStar  className="text-orange-500 w-[40px] text-orange-500  ml-[-5px]"  />
-               <FaStar  className="text-orange-500 w-[40px] text-orange-500  ml-[-5px]"  />
-               <FaStar  className="text-orange-500 w-[40px] text-orange-500  ml-[-5px]"  />
-
-           </div>
-           <p className="mt-5 text-xl md:text-2xl font-bold">
-           Short summary
-          </p>
-
-          <p
-          className="text-[var(--textColor)] text-[15px] mb-[50px] md:text-[18px] text-justify "
-          dangerouslySetInnerHTML={{ __html: data.desc }} 
-         />      
-
-      </div>
-      <div className="lg:w-1/4 flex flex-col gap-2">
-      <p className="text-md md:text-xl  font-bold ">
-        Table of contents
-      </p>
-
-      <p className="text-md md:text-xl mt-[20px] font-bold ">
-        What you  will learn
-      </p>
-        </div>
-     
-      </div>
-      {/* content */}
-      <div className="flex flex-col md:flex-row gap-12 justify-between">
-        {/* text */}
-        <div className="lg:text-lg flex flex-col gap-6 text-justify">
-         
-      
-        </div>
-        {/* menu */}
-        <div className="px-4 h-max sticky top-8">
-          <h1 className="mb-4 text-sm font-medium">Author</h1>
-          <div className="flex flex-col gap-4">
-            <div className="flex items-center gap-8">
-              {data.user.img && (
-                <Image
-                  src={data.user.img}
-                  className="w-12 h-12 rounded-full object-cover"
-                  w="48"
-                  h="48"
-                />
-              )}
-              <Link className="text-blue-800">{data.user.username}</Link>
+      <div className="flex flex-col shadow-2xl p-[1px] md:p-8 mt-[10px] gap-4">
+        <div className="flex flex-col md:flex-row gap-4 md:gap-8">
+          {data.img && (
+            <div className="w-full md:w-1/4 flex justify-center md:justify-start">
+              <Image src={data.img} w="400" className="rounded-2xl" />
             </div>
-            <p className="text-sm text-gray-500">
-              Lorem ipsum dolor sit amet consectetur
-            </p>
-            <div className="flex gap-2">
-            
+          )}
+
+          <div className="w-full md:w-2/4 flex flex-col gap-2 text-center md:text-left">
+            <p className="text-[var(--softTextColor3)]">SUMMARY OF</p>
+            <h1 className="text-xl md:text-4xl font-semibold">{data.title}</h1>
+            <div className="flex flex-col md:flex-row items-center md:items-start gap-2 text-[var(--textColor)] text-md md:text-lg">
+              <span>Book by</span>
+              <Link>{data.user.username}</Link>
+              <span>Category</span>
+              <Link>{data.category}</Link>
+            </div>
+            <div className="flex justify-center md:justify-start gap-1">
+              <FaStar className="text-orange-500 w-6 md:w-[40px] ml-[-5px]" />
+              <FaStar className="text-orange-500 w-6 md:w-[40px] ml-[-5px]" />
+              <FaStar className="text-orange-500 w-6 md:w-[40px] ml-[-5px]" />
+              <FaStar className="text-orange-500 w-6 md:w-[40px] ml-[-5px]" />
+              <FaStar className="text-orange-500 w-6 md:w-[40px] ml-[-5px]" />
             </div>
           </div>
-          <PostMenuActions post={data}/>
-         
+
+          <div className="w-full md:w-1/4 flex flex-col gap-2">
+            <p className="text-md md:text-xl font-bold">Table of contents</p>
+            <PostMenuActions post={data} />
+
+            <p className="text-md md:text-xl mt-[20px] font-bold">What you will learn</p>
+            <Comments postId={data._id} />
+          </div>
         </div>
       </div>
-      <Comments postId={data._id}/>
     </div>
-    </div>
-
   );
 };
 
