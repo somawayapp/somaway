98e0b4c628939984e4201dc2dc8a0b51aadd2d89
airnesygroup <airnesy.info@gmail.com>
2025-01-27 18:54:56 +0300

    new   commit

--- client/src/components/MobileControls.jsx
+++ client/src/components/MobileControls.jsx
@@ -3,7 +3,7 @@ import { Home, Compass, User } from "lucide-react";
 
 const MobileControls = () => {
   return (
-    <div className="fixed bottom-0left-1/2 transform -translate-x-1/2 flex justify-between bg-gray-800 p-4 rounded-2xl shadow-lg w-[90%] max-w-md">
+    <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2 flex justify-between bg-gray-800 p-4 rounded-2xl shadow-lg w-[90%] max-w-md">
       {/* Home Button */}
       <button className="flex flex-col items-center text-white hover:text-orange-500 transition">
         <Home className="h-6 w-6" />
