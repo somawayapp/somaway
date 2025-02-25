b5d125fbacba61f81dfac712ecf715748bb6237f
airnesygroup <airnesy.info@gmail.com>
2025-02-25 09:20:34 +0300

    first commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
     <div className="flex flex-col mt-0 md:mt-4">
       <div className="flex flex-col lg:flex-row lg:h-[60vh] gap-[100px] mt-4">
         {/* Left Section */}
-        <div className="lg:w-1/2 flex flex-col mt-0 md:mt-9 items-start gap-1 md:gap-4 rounded-md">
+        <div className="lg:w-2/5 flex flex-col mt-0 md:mt-9 items-start gap-1 md:gap-4 rounded-md">
           <h1 className="text-4xl lg:text-5xl font-bold text-[var(--textColor)]">
             #1 most
           </h1>
@@ -34,9 +34,9 @@ const Hero = () => {
         </div>
 
         {/* Right Section */}
-        <div className="lg:w-1/2 flex mt-[-53px] md:mt-0 flex-col">
+        <div className="lg:w-3/5 flex mt-[-53px] md:mt-0 flex-col">
           {/* First Featured Post */}
-          <div className="flex mb-8 md:mb-0 top-0">
+          <div className="flex mb-8 md:mb-[50px] top-0">
               <img
                 src="/desktop.webp"
                 className="hidden sm:block w-full object-cover rounded-lg"
