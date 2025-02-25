ccf8bb8a60cde68d4cd456761621bd6df289e3da
airnesygroup <airnesy.info@gmail.com>
2025-02-25 10:44:45 +0300

    first commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
     <div className="flex mb-0 md:mb-[50px] flex-col mt-0 md:mt-4">
       <div className="flex flex-col lg:flex-row lg:h-[60vh] gap-[100px] mt-4">
         {/* Left Section */}
-        <div className="lg:w-3/6 flex flex-col mt-0 md:mt-9 items-start gap-1 md:gap-4 rounded-md">
+        <div className="lg:w-4/9 flex flex-col mt-0 md:mt-9 items-start gap-1 md:gap-4 rounded-md">
           <h1 className="text-4xl lg:text-5xl font-bold text-[var(--textColor)]">
             #1 most
           </h1>
@@ -32,9 +32,10 @@ const Hero = () => {
             Get Started
           </Link>
         </div>
-       
+        <div className="lg:w-1/9 flex mt-[-53px] md:mt-[-40px] flex-col">
+          </div>
         {/* Right Section */}
-        <div className="lg:w-3/6 flex mt-[-53px] md:mt-[-40px] flex-col">
+        <div className="lg:w-4/9 flex mt-[-53px] md:mt-[-40px] flex-col">
           {/* First Featured Post */}
           <div className="flex mb-8 md:mb-[50px] top-0">
               <img
