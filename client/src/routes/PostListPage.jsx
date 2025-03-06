8c6e2e3d916ebab3d6d84d370802cee3bb588852
airnesygroup <airnesy.info@gmail.com>
2025-03-06 17:48:33 +0300

    first commit

--- client/src/routes/PostListPage.jsx
+++ client/src/routes/PostListPage.jsx
@@ -8,6 +8,11 @@ import Discover from "../components/Discover";
 import Footer from "../components/Footer";
 import Navbar from "../components/navbar2";
 import PopularPosts from "../components/PopularPosts";
+import LatestPosts from "../components/LatestPosts";
+import TrendingPosts from "../components/TrendingPosts";
+import FeaturedPosts from "../components/FeaturedPosts";
+import PostList from "../components/PostList";
+
 
 const PostListPage = () => {
   const [open, setOpen] = useState(false);
@@ -30,7 +35,7 @@ const PostListPage = () => {
     cat ? `Category: ${cat}` : "", // Display 'cat' if present
   ]
     .filter(Boolean) // Remove empty strings
-    .join(" | ") || "Most popular summaries"; // Default to "All Books" if no filters are applied
+    .join(" | ") || "Featured book summaries"; // Default to "All Books" if no filters are applied
 
   return (
     <div  className=" bg-[var(--navBg)] mb-[80px]  ">
@@ -78,20 +83,107 @@ const PostListPage = () => {
       </h1>
       <Search />
 
-</div>
-<PopularPosts />
-
+</div>   
+   <FeaturedPosts />
 
     </div>
 
 </div>
 
-<div className=" mt-4 mt-8 p-3 md:p-8">
-<h3 className="text-3xl md:text-5xl ml-2 mb-5 md:mb-9  font-bold text-[var(--textColor)]">
-All new releases
-</h3>
-<Discover />
+<div className=" mt-2 md:mt-4  p-3 md:p-8">
 
+   
+
+  
+<div >
+      <h3 className="text-xl md:text-3xl mt-7  md:mt-10 font-semibold text-[var(--textColor)]">
+     Trending Book summaries    </h3>
+    </div>
+    <TrendingPosts/>
+</div>
+
+
+
+    <div className="mt-0 mb:mt-[45px] ">
+      <div className="flex justify-between  mt-4 mb-15  md:mb-[40px] pt-5 pl-0  md:pl-5 overflow-x-hidden  rounded-2xl bg-[#7a00da] 
+        items-center gap-5 flex-col md:flex-row">
+      <div>
+      <h1 className="my-8 text-center lg:text-5xl text-2xl ml-2 pl-2 md:pl-0 mb-2 mt-4 lg:mb-5 lg:mt-8 text-white font-bold">
+      Get smarter in just 5 minutes</h1>
+      <p className="text-white pl-2 md:pl-0 text-center md:text-left pr-4 ml-2 text-md mb-5 md:mb-7 md:text-xl">Grasp the book’s key ideas in less than 5 minutes
+
+</p>
+<div className="flex justify-center md:justify-start">
+<Link
+            to="/login"
+            className="w-full items-center ml-4 md:ml-2 mr-4 text-center  text-md md:text-xl sm:w-auto px-4 md:px-6  py-3 md:py-3 bg-white 
+            text-black font-semibold  rounded-md hover:text-white hover:bg-black"
+          >
+            Get Started    
+          </Link>
+</div>
+    
+    </div>
+
+     <img
+            src="/group.svg"
+            className="w-100 md:w-180  h-40 md:h-80  md:mr-[40px] mr-0 object-cover "
+          />   
+          
+          </div>
+      </div>
+
+
+
+    <div className=" bg-[var(--bodyBg)] rounded-2xl p-3 mt-4 md:mt-8  mb-4 md:mb-8 md:p-6 ">
+    <h3 className="text-xl md:text-3xl  mt-5 mb-5 md:mb-9 md:mt-9   font-semibold text-[var(--bg)]">
+     Most popular summaries      </h3>
+    <PopularPosts/>
+    </div>
+
+
+ 
+
+<div className="mb-0 md:mb-[20px] ">
+<div>
+      <h3 className="text-xl md:text-3xl ml-2 mt-5 mb-5 md:mb-9 md:mt-9 font-semibold text-[var(--textColor)]">
+     Latest Book summaries     </h3>
+    </div>
+   <LatestPosts />
+</div>
+   
+
+
+
+
+
+      {/* Recent Posts */}
+      <div>
+      <div className="flex justify-between  mt-10 mb-10 md:mt[60px] md:mb-[75px] pt-5 pl-0  md:pl-5 overflow-x-hidden  rounded-2xl bg-[var(--textLogo)] 
+        items-center gap-5 flex-col md:flex-row">
+      <div>
+      <h1 className="my-8 lg:text-6xl text-3xl ml-2 pl-2 md:pl-0 mb-2 mt-4 lg:mb-5 lg:mt-8 text-[var(--textColore2)] font-bold"> Book summaries library</h1>
+      <p className="text-[var(--textColore2)] pl-2 md:pl-0 ml-2 text-md mb-5 md:mb-7 md:text-xl">Enjoy summarized nonfiction bestsellers</p>
+      <Link
+            to="/discover"
+            className="w-full ml-4 md:ml-2 text-center  text-md md:text-xl sm:w-auto px-4 md:px-6  py-3 md:py-3 bg-[#0062e3]   text-white font-semibold 
+            rounded-md hover:bg-[#0053bf]   "
+          >
+            Discover    
+                  </Link>
+    </div>
+
+     <img
+            src="/summary.svg"
+            className="w-100 md:w-180  h-40 md:h-80 mr-0  md:mr-[-100px] object-cover "
+          />   
+          
+          </div>
+
+    
+          <h3 className="text-2xl md:text-4xl ml-2 mt-5 mb-5 md:mb-9 md:mt-9 font-semibold text-[var(--textColor)]">
+          All book summaries     </h3>
+      <PostList />
 </div>
 
  </div>
