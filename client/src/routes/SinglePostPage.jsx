8e665b3278d9778d778e5c1edc62e468c7246486
airnesygroup <airnesy.info@gmail.com>
2025-03-07 14:36:18 +0300

    first commit

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -48,7 +48,7 @@ const SinglePostPage = () => {
 
 
 
-      <div className="flex flex-col bg-[var(--bg3)]  border border-[var(--softBg4)]  rounded-3xl  p-2 md:p-8
+      <div className="flex flex-col bg-[var(--bd3)]  border border-[var(--softBg4)]  rounded-3xl  p-2 md:p-8
        md:flex-row gap-4 md:gap-8">
           {data.img && (
             <div className="w-full md:w-1/4 mt-2 md:mt-0 flex justify-center md:block">
