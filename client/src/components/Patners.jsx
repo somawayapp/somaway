9a8e60716328eeac5e4557fde3f9f6fba7fc4fdc
airnesygroup <airnesy.info@gmail.com>
2025-03-07 11:29:48 +0300

    first commit

--- client/src/components/Patners.jsx
+++ client/src/components/Patners.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const Partners = () => {
   return (
     <div className="overflow-hidden whitespace-nowrap py-4 bg-white">
-      <div className="flex gap-8 animate-scroll">
-        <img src="/enterpreneur.svg" className="h-9 md:h-[50px]" />
-        <img src="/euronews.svg" className="h-9 md:h-[50px]" />
-        <img src="/forbes.svg" className="h-9 md:h-[50px]" />
-        <img src="/muo.svg" className="h-9 md:h-[50px]" />
-        <img src="/reuters.svg" className="h-9 md:h-[50px]" />
-        <img src="/yahoo.svg" className="h-9 md:h-[50px]" />
+      <div className="flex  gap-9  md:gap-[80px] animate-scroll">
+        <img src="/enterpreneur.svg" className="h-6 md:h-[30px]" />
+        <img src="/euronews.svg" className="h-6 md:h-[30px]" />
+        <img src="/forbes.svg" className="h-6 md:h-[30px]" />
+        <img src="/muo.svg" className="h-6 md:h-[30px]" />
+        <img src="/reuters.svg" className="h-6 md:h-[30px]" />
+        <img src="/yahoo.svg" className="h-6 md:h-[30px]" />
         {/* Duplicate the logos for seamless scrolling */}
-        <img src="/enterpreneur.svg" className="h-9 md:h-[50px]" />
-        <img src="/euronews.svg" className="h-9 md:h-[50px]" />
-        <img src="/forbes.svg" className="h-9 md:h-[50px]" />
-        <img src="/muo.svg" className="h-9 md:h-[50px]" />
-        <img src="/reuters.svg" className="h-9 md:h-[50px]" />
-        <img src="/yahoo.svg" className="h-9 md:h-[50px]" />
+        <img src="/enterpreneur.svg" className="h-6 md:h-[30px]" />
+        <img src="/euronews.svg" className="h-6 md:h-[30px]" />
+        <img src="/forbes.svg" className="h-6 md:h-[30px]" />
+        <img src="/muo.svg" className="h-6 md:h-[30px]" />
+        <img src="/reuters.svg" className="h-6 md:h-[30px]" />
+        <img src="/yahoo.svg" className="h-6 md:h-[30px]" />
       </div>
     </div>
   );
@@ -27,12 +27,3 @@ export default Partners;
 // Tailwind CSS Styles (Add to your global CSS or tailwind config)
 // Add this to your CSS file or global styles
 // If using Tailwind, include this in your styles
-// @keyframes scroll {
-//   from { transform: translateX(0); }
-//   to { transform: translateX(-50%); }
-// }
-
-// .animate-scroll {
-//   display: flex;
-//   animation: scroll 20s linear infinite;
-// }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,13 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        scroll: "scroll 20s linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-50%)" },
+        },
+      },
+    },
   },
-  plugins: [],
   plugins: [require("tailwind-scrollbar-hide")],
-
-
 };
+
 
 
 
