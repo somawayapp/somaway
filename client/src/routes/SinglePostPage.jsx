f5eca5a35058e4fc95d3a01fe2ebcf8fac434483
airnesygroup <airnesy.info@gmail.com>
2025-03-12 10:09:53 +0300

    first commit

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -11,6 +11,7 @@ import Footer from "../components/Footer";
 import ExplorePosts from "../components/ExplorePosts";
 import MobileControls from "../components/MobileControls";
 import LatestPosts from "../components/LatestPosts";
+import { useEffect } from "react";
 
 const fetchPost = async (slug) => {
     useEffect(() => {
