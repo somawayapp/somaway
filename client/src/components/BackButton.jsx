591940989016d44ef286060b7cdd3394514ccb88
airnesygroup <airnesy.info@gmail.com>
2025-03-21 22:04:24 +0300

    Initial commit with new code

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -1,17 +1,14 @@
 "use client";
 import React from "react";
 import { MdKeyboardBackspace } from "react-icons/md";
-import { useMoveBack } from "@/hooks/useMoveBack";
 
 const BackButton = () => {
-  const back = useMoveBack();
   return (
     <button
       type="button"
       className="flex flex-row gap-2 items-center text-[15px] font-semibold py-2 px-4 rounded-full hover:bg-neutral-100 transition
        cursor-pointer text--[var(--textColor)] "
-      onClick={back}
-    >
+       onClick={() => window.history.back()}>
     <MdKeyboardBackspace size={18}/>
       <span>
         Back
