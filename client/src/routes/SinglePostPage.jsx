fe5df4c76fb38474c145bcf2275bd39f6cf06155
airnesygroup <airnesy.info@gmail.com>
2025-03-21 13:58:19 +0300

    Initial commit with new code

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -70,19 +70,21 @@ const SinglePostPage = () => {
 
       <div className="flex flex-col bg-[var(--bd3)]  border border-[var(--softBg4)]  rounded-3xl  p-2 md:p-8
        md:flex-row gap-4 md:gap-8">
-          
-                {data.images && Array.isArray(data.images) && data.images.length > 0 && (
-                  <div className="w-full md:w-1/4 mt-2 md:mt-0 flex justify-center md:block">
-                    {data.images.map((images, index) => (
-                      <Image
-                        key={index}
-                        src={images}
-                        className="w-[180px] md:w-[400px] rounded-2xl mb-2"
-                      />
-                    ))}
-                  </div>
-              
-          )}
+      {data.images && Array.isArray(data.images) && data.images.length > 0 && (
+  <div className="w-full md:w-1/4 mt-2 md:mt-0 flex justify-center md:block">
+    {data.images.map((image, index) => (
+      <Image
+        key={index}
+        src={image}
+        width={400} // Set explicit width
+        height={400} // Set explicit height
+        className="w-[180px] md:w-[400px] rounded-2xl"
+        alt={`Image ${index}`}
+      />
+    ))}
+  </div>
+)}
+
 
           <div className="flex flex-col gap-1 md:gap-2 items-center md:items-start md:w-2/4">
             <p className="text-[var(--softTextColor2)] text-sm text-lg  text-center md:text-left">
