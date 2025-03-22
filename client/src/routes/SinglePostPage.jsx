109606a087b83d1a82aa3857b5fabdc1b2bd8ad0
airnesygroup <airnesy.info@gmail.com>
2025-03-22 12:36:55 +0300

    Initial commit with new code

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -187,11 +187,8 @@
     
 
 
+          <div className="flex flex-col md:flex-row-reverse bg-[var(--bg)] mt-6 gap-4 md:gap-8">
 
-
-
-      <div className="flex flex-col bg-[var(--bg)]  mt-6 
-       md:flex-row gap-4 md:gap-8">
 
 
 
@@ -203,7 +200,7 @@
         <div className="text-[16px] font-semibold flex flex-row items-center gap-2">
         <span className="mr-1">Hosted by</span> 
         <img
-  className="rounded-full h-[20px] md:h-[30px] select-none"
+  className="rounded-full w-[20px]  md:w-[40px] h-[20px]  md:h-[40px] select-none"
   alt="Avatar"
   src={data.user?.img || "/placeholder.webp"} 
 />
