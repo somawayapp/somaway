2731b098086d8377fdccd0f0fa963af2024f239a
airnesygroup <airnesy.info@gmail.com>
2025-03-23 14:49:15 +0300

    Initial commit with new code

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,7 +13,8 @@ import webhookRouter from '../routes/webhook.route.js';
 import subscriptionRouter from '../routes/subscription.route.js';
 import cors from 'cors';
 import 'dotenv/config';
-import router from '../routes/rating.route.js';
+import ratingRouter from '../routes/rating.route.js';
+
 dotenv.config();
 
 const app = express();
@@ -82,7 +83,7 @@ app.use('/posts', postRouter); // Correctly map the posts route
 app.use('/subscriptions', subscriptionRouter);
 app.use('/comments', commentRouter);
 app.use('/webhook', webhookRouter);
-app.use('/ratings', router);
+app.use('/ratings', ratingRouter);
 
 // Debug route to confirm server is running
 app.get('/debug', (req, res) => {
