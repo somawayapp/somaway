3aa459047c0fa3ee3702fb8181c601c51810b17e
airnesygroup <airnesy.info@gmail.com>
2025-03-23 13:14:19 +0300

    Initial commit with new code

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -214,7 +214,7 @@ const details = [
 {/* Right Side Images */}
 <div className="w-1/4 h-full flex flex-col overflow-hidden relative">
   <div ref={rightDivRef} className="flex flex-col gap-2 h-full">
-    {sideImages.slice(0, showMore ? sideImages.length : 6).map((image, index) => (
+    {sideImages.slice(0, showMore ? sideImages.length : 5).map((image, index) => (
       <div
         key={index}
         className="w-full h-1/4 overflow-hidden relative cursor-pointer"
@@ -223,14 +223,15 @@ const details = [
         <img src={image} className="object-cover h-full w-full" alt={`Image ${index + 1}`} />
 
         {/* Floating Show More Button on the 8th Image */}
-        {index === 5 && !showMore && (
-       <button
-       className="absolute inset-0 m-auto flex-col items-center w-[20px] h-[10px] justify-center px-4 py-2 rounded-xl border-[1px] border-white bg-black
-        bg-opacity-60 text-white text-xs md:text-sm hover:bg-opacity-80 transition whitespace-nowrap"
-       onClick={() => openPopup(index === 6)}
-     >
-       show more
-     </button>
+        {index === 4 && !showMore && (
+   <button
+   className="absolute inset-0 m-auto flex items-center justify-center w-[80px] h-[40px] px-4 py-2 rounded-xl border-[1px] border-white bg-black
+   bg-opacity-60 text-white text-xs md:text-sm hover:bg-opacity-80 transition whitespace-nowrap"
+   onClick={() => openPopup(index === 7)}
+ >
+   show more
+ </button>
+ 
      
         )}
       </div>
