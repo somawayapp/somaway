3e66f4176024cc8e4fc1fde60f698a1e256fd9ca
airnesygroup <airnesy.info@gmail.com>
2025-03-24 15:52:13 +0300

    Initial commit with new code

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -53,45 +53,64 @@ const Search = () => {
 
   return (
     <>
-     <button
+         <button
       type="button"
       className="border-[1px] border-[var(--softBg4)] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition duration-300 cursor-pointer"
       onClick={() => setIsOpen(true)}
     >
-      <div className="flex flex-row justify-between items-center gap-2 relative">
+      <div className="flex flex-row justify-between items-center relative gap-4">
         {/* Location */}
-        <div className="flex items-center justify-center group hover:bg-[var(--softBg4)] px-3 rounded-full">
+        <div className="relative group hover:bg-[var(--softBg4)] rounded-full flex items-center gap-2 px-4">
           <small className="text-[12px] md:text-[14px] font-semibold text-[var(--softTextColor)] group-hover:bg-[var(--softBg4)] py-2 transition">
             {filters.location || "Anywhere"}
           </small>
           <span className="text-[14px] text-[var(--softTextColor)]">|</span>
-        </div>
-
-        {/* Property Type & Size */}
-        <div className="flex items-center justify-center group hover:bg-[var(--softBg4)] px-3 rounded-full">
-          <small className="font-semibold text-[12px] md:text-[14px] text-[var(--softTextColor)] group-hover:bg-[var(--softBg4)] py-2 transition">
-            {filters.propertysize || "Any size"}
-          </small>
-          <span className="text-[14px] text-[var(--softTextColor)]">|</span>
+          <span className="text-[14px] text-[var(--softerTextColor)] whitespace-nowrap">
+            Choose a location
+          </span>
         </div>
 
         {/* Price */}
-        <div className="flex items-center justify-center group hover:bg-[var(--softBg4)] px-3 rounded-full">
+        <div className="relative group hover:bg-[var(--softBg4)] rounded-full flex items-center gap-2 px-4 border-x-[1px] border-[var(--softBg4)]">
           <small className="text-[12px] md:text-[14px] font-semibold text-[var(--softTextColor)] group-hover:bg-[var(--softBg4)] py-2 transition">
-            {filters.pricemin || filters.pricemax ? `${filters.pricemin ? `KSh ${filters.pricemin}` : "KSh 0"} - ${filters.pricemax ? `KSh ${filters.pricemax}` : "KSh 0"}` : "Any price"}
+            {filters.pricemin || filters.pricemax ? (
+              <>
+                {filters.pricemin ? `KSh ${filters.pricemin}` : "KSh 0"} - {filters.pricemax ? `KSh ${filters.pricemax}` : "KSh 0"}
+              </>
+            ) : (
+              "Any price"
+            )}
           </small>
           <span className="text-[14px] text-[var(--softTextColor)]">|</span>
+          <span className="text-[14px] text-[var(--softerTextColor)] whitespace-nowrap">
+            Set price range
+          </span>
+        </div>
+
+        {/* Property Size */}
+        <div className="relative group hover:bg-[var(--softBg4)] rounded-full flex items-center gap-2 px-4">
+          <small className="text-[12px] md:text-[14px] font-semibold text-[var(--softTextColor)] group-hover:bg-[var(--softBg4)] py-2 transition">
+            {filters.propertysize || "Any size"}
+          </small>
+          <span className="text-[14px] text-[var(--softTextColor)]">|</span>
+          <span className="text-[14px] text-[var(--softerTextColor)] whitespace-nowrap">
+            Select property size
+          </span>
         </div>
 
         {/* Property Type */}
-        <div className="flex items-center justify-center group hover:bg-[var(--softBg4)] px-3 rounded-full">
-          <small className="font-semibold text-[12px] md:text-[14px] text-[var(--softTextColor)] group-hover:bg-[var(--softBg4)] py-2 transition">
+        <div className="relative group hover:bg-[var(--softBg4)] rounded-full flex items-center gap-2 px-4">
+          <small className="text-[12px] md:text-[14px] font-semibold text-[var(--softTextColor)] group-hover:bg-[var(--softBg4)] py-2 transition">
             {filters.propertytype || "Any type"}
           </small>
+          <span className="text-[14px] text-[var(--softTextColor)]">|</span>
+          <span className="text-[14px] text-[var(--softerTextColor)] whitespace-nowrap">
+            Select property type
+          </span>
         </div>
 
         {/* Search Icon */}
-        <div className="p-3 md:p-4 bg-[#fc3239] rounded-full text-white transition-transform duration-300 hover:scale-110 hover:bg-[#d82930]">
+        <div className="p-3 md:p-4 ml-0 md:ml-[80px] bg-[#fc3239] rounded-full text-white transition-transform duration-300 hover:scale-110 hover:bg-[#d82930]">
           <FaSearch className="text-[12px] md:text-[14px]" />
         </div>
       </div>
