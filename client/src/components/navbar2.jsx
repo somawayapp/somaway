6a525e6927ea54745f19cf77bb429dc86fd451eb
airnesygroup <airnesy.info@gmail.com>
2025-03-24 10:04:03 +0300

    Initial commit with new code

--- client/src/components/navbar2.jsx
+++ client/src/components/navbar2.jsx
@@ -97,11 +97,10 @@ const Navbar = () => {
 
 
 
-    {/* Person icon */}
     <div className="rounded-full bg-[var(--softTextColor)] items-center justify-center">
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    className="w-[60px] h-[60px] mb-[-10px]"
+    className="w-[60px] h-[60px] mb-[-12px]"
     viewBox="0 0 24 24"
     fill="var(--bg)"
   >
