5bdeecba5bfbb27c91872a741fd9eab75e55c01a
airnesygroup <airnesy.info@gmail.com>
2025-03-25 17:04:50 +0300

    Initial commit with new code

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,7 +1,5 @@
-import { useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { FaSearch, FaTimes } from "react-icons/fa";
-import { useRef } from "react";
 
 
 
