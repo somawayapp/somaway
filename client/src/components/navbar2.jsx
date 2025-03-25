83805b740213342560f33b17c6f4a3be5d7bad11
airnesygroup <airnesy.info@gmail.com>
2025-03-25 19:36:17 +0300

    Initial commit with new code

--- client/src/components/navbar2.jsx
+++ client/src/components/navbar2.jsx
@@ -56,60 +56,51 @@
         document.body.style.overflow = "auto";
       }
     }, [isOpen]);
+
+
+
   
-    return(
-  <div 
-  style={{
-    zIndex: 100004,
-  }}
-  className="flex justify-center items-center flex-col relative w-full px-3 md:px-[80px] gap-3 py-2  md:gap-6 flex sticky
-         top-0  text-[var(--TextColor)] bg-[var(--bg)] md:border-b  border-b-[var(--softBg4)] "
-     >
+          return(
+          <div  style={{ zIndex: 100004, }}
+             className="flex justify-center items-center flex-col relative w-full px-3 md:px-[80px] gap-3 py-2  md:gap-6 flex sticky top-0  text-[var(--TextColor)] bg-[var(--bg)] md:border-b  border-b-[var(--softBg4)] " >
       
       
-      <div
-       className="relative w-full  mt-2 gap-3 md:gap-6 flex flex-row items-center  text-[var(--TextColor)]  justify-between  bg-[var(--bg)] "
-     >
+            <div className="relative w-full  mt-2 gap-3 md:gap-6 flex flex-row items-center  text-[var(--TextColor)]  justify-between  bg-[var(--bg)] "           >
   
   
-    {/* Show on medium screens and larger */}
-    <Link to="/" className="flex items-center gap-1 text-xl font-bold md:text-3xl hidden md:flex">
-      <img src="/airlogo2.png" className="md:h-8" />
-    </Link>
+             {/* Show on medium screens and larger */}
+             <Link to="/" className="flex items-center gap-1 text-xl font-bold md:text-3xl hidden md:flex">
+              <img src="/airlogo2.png" className="md:h-8" />
+             </Link>
     
   
-  
 
-
-
-
-
-       
-  
-       <div className="flex items-center justify-between flex-row gap-2 md:gap-9">
-  
-  
-    <div className="relative"  onClick={() => setOpen((prev) => !prev)}>
-        <div className="flex flex-row items-center gap-3">
+             <div className="flex items-center justify-between flex-row gap-2 md:gap-9">
+              <div className="relative"  onClick={() => setOpen((prev) => !prev)}>
+              <div className="flex flex-row items-center gap-3">
               <button
                 type="button"
                 className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-[var(--softBg4)] transition cursor-pointer text-[var(--softBg5)]"
               >
                 Add listng
-              </button>
+                 </button>
                 <button
                   type="button"
                   className=" p-4 md:py-1 md:pl-4 md:pr-2 border-[1px]  border-[var(--softBg4)]  flex  flex-row  items-center   gap-3   rounded-full   cursor-pointer   hover:shadow-md   transition duration-300"
                 >
                   <AiOutlineMenu />
                   <div className="hidden md:flex items-center justify-center">
-    <SignedIn>
-      <div className="flex items-center  w-[38px] h-[38px] justify-center">
-        <UserButton />
-      </div>
-    </SignedIn>
+          
+          
+           <SignedIn>
+            <div className="flex items-center  w-[38px] h-[38px] justify-center">
+           <UserButton />
+           </div>
+           </SignedIn>
   
-    <SignedOut>
+          
+          
+           <SignedOut>
            <img
             className="rounded-full text-[14px] w-[38px] h-[38px] select-none"
             alt="Avatar"
@@ -210,7 +201,7 @@
 
             <Link
               to="/premium"
-              className="block py-2 text-[var(--TextColor)] font-bold  hover:text-[#fc3239]  p-2 rounded-xl"
+              className="block py-2 text-[var(--softTextColor)] font-bold  hover:text-[#fc3239]  p-2 rounded-xl"
               onClick={() => setOpen(false)}              > Pricing       
               </Link>
 
@@ -226,21 +217,24 @@
 
             <Link
               to="/help"
-              className="block py-2 text-[var(--TextColor)] font-bold   hover:text-[#fc3239]  p-2 rounded-xl"
+              className="block py-2 text-[var(--softTextColor)] font-bold   hover:text-[#fc3239]  p-2 rounded-xl"
              onClick={() => setOpen(false)}              > help center      
               </Link>
             
              <Link
               to="/settings"
-              className="block py-2 text-[var(--TextColor)] font-bold  hover:text-[#fc3239]  p-2 rounded-xl"
+              className="block py-2 text-[var(--softTextColor)] font-bold  hover:text-[#fc3239]  p-2 rounded-xl"
               onClick={() => setOpen(false)}              > Settings     
                   </Link>
               </div>
           
           </div>
   
-        </div>
+           </div>
 
+            <div>
+              <Search/>
+            </div>
         </div>
     );
   };
