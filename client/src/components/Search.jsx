e8a8512eecedba298a6038f1e72420f0590c2728
airnesygroup <airnesy.info@gmail.com>
2025-03-25 10:08:26 +0300

    Initial commit with new code

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -74,16 +74,13 @@ const Search = () => {
   return(
     <div>
   <div
-  className="w-full z-50 hidden md:flex flex-col gap-4 items-center justify-center transition-all duration-300 "
+  className="w-full z-50 hidden md:flex flex-col gap-6 mt-[-60px] items-center justify-center transition-all duration-300 "
     >
       {/* Rent & Sale Links */}
       {!isScrolledUp && (
 
       <div
-        className={`flex justify-center gap-[40px] transition-all duration-300 ${
-          isScrolledUp ? "opacity-0 translate-y-[-20px] pointer-events-none" : "opacity-100 translate-y-0"
-        }`}
-      >
+        className="flex justify-center gap-[40px] transition-all duration-300 ">
         <a href="?model=forrent" className="text-[16px] font-semibold text-[var(--softTextColor)] hover:underline">
           For Rent
         </a>
@@ -101,7 +98,7 @@ const Search = () => {
   onClick={() => setIsOpen(true)}
   type="button"
   className={`border-[1px] border-[var(--softBg4)] w-full mx-auto rounded-full shadow-sm hover:shadow-md transition duration-300 cursor-pointer ${
-    isScrolledUp ? "translate-y-[-75px] " : "translate-y-0"
+    isScrolledUp ? "translate-y-[-20px] " : "translate-y-0"
   }`}
 >
   <div className="flex flex-row  items-center relative gap-2 justify-between">
