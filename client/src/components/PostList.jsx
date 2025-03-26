47894cba75f56ebca0976838c78d5765fd8e6057
airnesygroup <airnesy.info@gmail.com>
2025-03-27 00:24:34 +0300

    Initial commit with new code

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -78,17 +78,18 @@ const PostList = () => {
   
     return (
     
-    <div className="gap-2 grid grid-cols-1 md:grid-cols-4 md:gap-6 scrollbar-hide">
-      {displayedPosts.map((post) => (
-        <PostListItem key={post._id} post={post} />
-      ))}
-
-       {Array(12).fill(0).map((_, index) => (
+      <div className="gap-2 grid grid-cols-1 md:grid-cols-4 md:gap-6 scrollbar-hide">
+      {displayedPosts.length > 0 ? (
+        displayedPosts.map((post) => <PostListItem key={post._id} post={post} />)
+      ) : (
+        Array(12).fill(0).map((_, index) => (
           <div key={index} className="relative aspect-[3/3] w-full h-full">
             <div className="absolute inset-0 bg-[var(--softBg4)] animate-pulse rounded-xl md:rounded-2xl"></div>
           </div>
-        ))}
+        ))
+      )}
     </div>
+    
   );
 };
 
