23403f1727cb2447f4a56d9e8641bbb18cc59bed
airnesygroup <airnesy.info@gmail.com>
2025-03-27 00:42:20 +0300

    Initial commit with new code

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -76,18 +76,26 @@ const PostList = () => {
     );
   }
   
+  
+  if (displayedPosts.length < 4) {
     return (
-    
-    <div className="gap-2 grid grid-cols-1 md:grid-cols-4 md:gap-6 scrollbar-hide">
-      {displayedPosts.map((post) => (
-        <PostListItem key={post._id} post={post} />
-      ))}
-
-       {Array(4).fill(0).map((_, index) => (
+      <div className="gap-2 grid grid-cols-1 md:grid-cols-4 md:gap-6 scrollbar-hide">
+        {Array(4).fill(0).map((_, index) => (
           <div key={index} className="relative aspect-[3/3] w-full h-full">
             <div className="absolute inset-0 bg-[var(--softBg4)] animate-pulse rounded-xl md:rounded-2xl"></div>
           </div>
         ))}
+      </div>
+    );
+  }
+  
+
+
+  return (
+    <div className="gap-2 grid grid-cols-1 md:grid-cols-4 md:gap-6 scrollbar-hide">
+      {displayedPosts.map((post) => (
+        <PostListItem key={post._id} post={post} />
+      ))}
     </div>
   );
 };
