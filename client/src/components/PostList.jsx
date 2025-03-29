c0e6c83b43e0e8e653d433749b732787a7b265d0
airnesygroup <airnesy.info@gmail.com>
2025-03-29 11:39:38 +0300

    Initial commit with new code

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -64,16 +64,16 @@ const PostList = () => {
   if (displayedPosts.length === 0 && showMessage) {
     return (
       <div className="flex flex-col items-center justify-center h-[50vh]">
-              <Link to={`/`}>
-        
-        <button
-          className="px-9 py-4 rounded-xl border border-[var(--softBg4)] text-[var(--softTextColor)] shadow-md hover:text-[var(--textColor)]"
-        >
-          <p className="mb-4 ">No posts found</p>
-          <p className="mb-4 font-bold ">
-            Go Back Home
-          </p>
-        </button>              </Link>
+       <Link to="/addlisting#review">
+  <button
+    className="w-full px-6 py-3 rounded-xl border border-[var(--softBg4)] 
+               text-[var(--softTextColor)] shadow-md 
+               hover:text-[var(--textColor)] text-center"
+  >
+    <p className="mb-2">No posts found</p>
+    <p className="mb-2 font-bold">Go back home</p>
+  </button>
+</Link>
 
       </div>
     );
