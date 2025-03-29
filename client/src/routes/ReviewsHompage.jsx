264e4532aea73c430cfd1263f32705e7a47354a7
airnesygroup <airnesy.info@gmail.com>
2025-03-29 14:49:13 +0300

    Initial commit with new code

--- client/src/routes/ReviewsHompage.jsx
+++ client/src/routes/ReviewsHompage.jsx
@@ -70,12 +70,12 @@ from ${author || 'top authors'}in minutes. Elevate your mind—only on Hodi!`} /
       <Link
         style={{ zIndex: 100004 }}
         to="/reviews"
-        className="px-5 md:px-8 py-5 md:py-8 bg-[var(--softTextColori)] text-[12px] md:text-[16px]
-       sticky  bottom-[60px]  md:bottom-[30px] left-1/2 -translate-x-1/2 rounded-full text-[var(--softBg)]
+        className="p-4 md:px-8  md:py-3 bg-[var(--softTextColori)] text-[12px] md:text-[16px]
+       sticky  bottom-[60px] left-9 md:left-[50%]  md:bottom-[30px] left-1/2 -translate-x-1/2 rounded-full text-[var(--softBg)]
         shadow-md font-semibold flex items-center gap-2 hover:bg-[var(--textColor)] "
       >         <Plus className="w-4 h-4 md:w-5 md:h-5" />
 
-        <p>Review</p>
+        <p className="hidden md:block">Review</p>
       </Link>
     </div>       
       </div>
