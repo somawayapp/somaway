ac08009f50a09c8f70ed2fd6d37fcbae36f9595b
airnesygroup <airnesy.info@gmail.com>
2025-04-03 16:00:28 +0300

    Initial commit with new code

--- client/src/routes/AddListing.jsx
+++ client/src/routes/AddListing.jsx
@@ -121,19 +121,18 @@ const AddListing = () => {
         <form onSubmit={handleSubmit} className="flex flex-col gap-6">
       
 
-        <Upload type="image" setProgress={setProgress} setData={setImg}>
-  <button className="p-2 bg-[var(--textColore)] text-[var(--textColor)] rounded-lg">
-    Upload Additional Images
-  </button>
-</Upload>
+        <div>
+      <Upload type="image" setProgress={setProgress} setData={setImg} />
 
-{img.length > 0 && (
-  <div className="flex flex-wrap gap-2">
-    {img.map((img, index) => (
-      <img key={index} src={img.url} alt="Uploaded" className="w-[100px] h-[100px] object-cover" />
-    ))}
-  </div>
-)}
+      {/* Display uploaded images */}
+      {img.length > 0 && (
+        <div className="flex flex-wrap gap-2 mt-4">
+          {img.map((img, index) => (
+            <img key={index} src={img.url} alt="Uploaded" className="w-[100px] h-[100px] object-cover rounded-lg" />
+          ))}
+        </div>
+      )}
+    </div>
 
 
 
