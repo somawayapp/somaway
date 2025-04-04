639311dff649fad9abeab65e09ae83e5b07c8328
airnesygroup <airnesy.info@gmail.com>
2025-04-04 13:21:44 +0300

    Initial commit with new code

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -19,7 +19,7 @@ const authenticator = async () => {
     const { signature, expire, token } = data;
     return { signature, expire, token };
   } catch (error) {
-    throw new Error(`Authentication quest failed: ${error.message}`);
+    throw new Error(`Authentication request failed: ${error.message}`);
   }
 };
 
