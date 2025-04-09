986b75e37857a63cd62d2744de103bce45e8a7b4
airnesygroup <airnesy.info@gmail.com>
2025-04-09 21:00:32 +0300

    Initial commit with new code

--- client/src/components/PropertySwitcher.jsx
+++ client/src/components/PropertySwitcher.jsx
@@ -12,18 +12,17 @@ export default function PropertySwitcher() {
     setToggleState(newState);
     navigate("/"); // Always go to home when toggling
   };
-
   const handleModelToggle = () => {
     const newState = !toggleModelState;
     setToggleModelState(newState);
     navigate(location.pathname + `?model=${newState ? 'forsale' : 'forrent'}`);
   };
-  
+
   const handleGoHome = () => {
     navigate("/");
   };
 
-  const isNotRootPath = location.pathname === "";
+  const isNotRootPath = location.pathname !== "/" && location.pathname !== "";
 
   const isRootPathWithoutSearchParams = location.pathname === "/" && !location.search;
 
@@ -88,40 +87,39 @@ export default function PropertySwitcher() {
           </div>
         </div>
 
-  
-{isRootPathWithoutSearchParams && (
-  <div className="flex justify-between block md:hidden gap-2 md:gap-4 items-center w-full max-w-sm">
-    
-    {/* Clickable text */}
-    <div onClick={handleModelToggle} className="cursor-pointer">
-      <p className="text-[16px] text-[var(--softTextColor)] font-bold">
-        Proprty model
-      </p>
-      <div className="flex">
-        <p className={`text-md text-[var(--softTextColor)] hover:underline ${!toggleModelState ? 'font-bold' : ''}`}>
-          For rent/
-        </p>
-        <p className={`text-sm md:hidden block text-[var(--softTextColor)] ${toggleModelState ? 'font-bold' : ''}`}>
-          For sale
-        </p>
-      </div>
-    </div>
+        {isRootPathWithoutSearchParams && (
 
-    {/* Fancy Toggle Switch */}
-    <div
-      onClick={handleModelToggle}
-      className="w-16 h-8 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-300 bg-[var(--softTextColor)]"
-    >
-      <div
-        className={`w-6 h-6 flex items-center justify-center text-[var(--softTextColor)] rounded-full shadow-md transform duration-300 ease-in-out
-          ${toggleModelState ? "translate-x-8 bg-[var(--bg)]" : "translate-x-0 bg-[var(--bg)]"}`}
-      >
-        {toggleState ? "✕" : "✓"}
-      </div>
-    </div>
-  </div>
-)}
+        <div className="flex justify-between block md:hidden  gap-2 md:gap-4 items-center w-full max-w-sm">
+          
+          {/* Clickable text */}
+          <div onClick={handleModelToggle} className="cursor-pointer ">
+            <p className="text-[16px] text-[var(--softTextColor)] font-bold">
+              Proprty model
+            </p>
+            <div className="flex">
+            <span className="text-md text-[var(--softTextColor)] hover:underline">
+              For rent/
+            </span>
+            <span className="text-sm md:hidden block text-[var(--softTextColor)]">
+              For sale
+            </span>
+            </div>
+       
+          </div>
 
+          {/* Fancy Toggle Switch */}
+          <div
+            onClick={handleModelToggle}
+            className="w-16 h-8 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-300 bg-[var(--softTextColor)]"
+          >
+            <div
+              className={`w-6 h-6 flex items-center justify-center text-[var(--softTextColor)] rounded-full shadow-md transform duration-300 ease-in-out
+                ${toggleModelState ? "translate-x-8 bg-[var(--bg)]" : "translate-x-0 bg-[var(--bg)]"}`}
+            >
+              {toggleState ? "✕" : "✓"}
+            </div>
+          </div>
+        </div> )}
 
     </div>
   );
