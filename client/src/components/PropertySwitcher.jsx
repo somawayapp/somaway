25846952ec5f3ae9c1f3c1fea460e39bc5719eed
airnesygroup <airnesy.info@gmail.com>
2025-04-09 20:35:05 +0300

    Initial commit with new code

--- client/src/components/PropertySwitcher.jsx
+++ client/src/components/PropertySwitcher.jsx
@@ -22,13 +22,45 @@ export default function PropertySwitcher() {
     navigate("/");
   };
 
+  const isRootPath = location.pathname === "/";
+
   
 
 
   return (
     <div className="w-full border p-3 border-[var(--softBg4)] rounded-xl mb-5 shadow-md flex flex-col items-center space-y-4">
-      {/* Extra options shown if not on root path */}
-        <div className="flex justify-between gap-2 md:gap-4 items-center w-full max-w-sm">
+        {isRootPath && (
+
+        <div className="flex justify-between gap-2 block md:hidden md:gap-4 items-center w-full max-w-sm">
+          
+          {/* Clickable text */}
+          <div onClick={handleGoHome} className="cursor-pointer">
+            <p className="text-md text-[var(--softTextColor)] font-bold hover:underline">
+              Remove all filters
+            </p>
+            <p className="text-sm md:hidden block text-[var(--softTextColor)] font-normal">
+              go back home
+            </p>
+          </div>
+
+          {/* Fancy Toggle Switch */}
+          <div
+            onClick={handleToggle}
+            className="w-16 h-8 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-300 bg-[var(--softTextColor)]"
+          >
+            <div
+              className={`w-6 h-6 flex items-center justify-center text-[var(--softTextColor)] rounded-full shadow-md transform duration-300 ease-in-out
+                ${toggleState ? "translate-x-8 bg-[var(--bg)]" : "translate-x-0 bg-[var(--bg)]"}`}
+            >
+              {toggleState ? "✕" : "✓"}
+            </div>
+          </div>
+        </div>    )}
+
+
+
+
+        <div className="flex justify-between hidden md:block gap-2 md:gap-4 items-center w-full max-w-sm">
           
           {/* Clickable text */}
           <div onClick={handleGoHome} className="cursor-pointer">
@@ -53,7 +85,6 @@ export default function PropertySwitcher() {
             </div>
           </div>
         </div>
-
 
 
         <div className="flex justify-between block md:hidden  gap-2 md:gap-4 items-center w-full max-w-sm">
