187e193c3f608daef2f4c6f278f230c3d188fd8f
airnesygroup <airnesy.info@gmail.com>
2025-04-12 14:36:52 +0300

    Initial commit with new code

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -139,22 +139,22 @@ const PostList = () => {
   }
 
   return (
-    <div className="gap-2 grid grid-cols-1 md:grid-cols-4 md:gap-6 scrollbar-hide">
+    <div className=" scrollbar-hide">
       {/* Display featured posts first on medium screens */}
+     
       {featuredPosts.length > 0 && (
-        <div className="md:col-span-4">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
             {featuredPosts.slice(0, 2).map((post) => (
               <PostListItem key={post._id} post={post} />
             ))}
 
+          </div>
+      )}
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
         {displayedPosts.map((post) => (
           <PostListItem key={post._id} post={post} />
         ))}
-          </div>
-        </div>
-      )}
-
+      </div>
 
     </div>
   );
