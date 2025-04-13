6e096f6c13ef7091a9d1deebdcc2901da5ee7a5e
airnesygroup <airnesy.info@gmail.com>
2025-04-13 16:30:37 +0300

    Initial commit with new code

--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -1,24 +1,22 @@
 import mongoose from "mongoose";
 
-// This function is used to connect to MongoDB using Mongoose
 const connectDB = async () => {
   try {
-    const uri =  "mongodb+srv://makesomaway:makesomaway@cluster0.movbe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&dbName=app "; // Get the DB connection URI from environment variables
+    const uri = "mongodb+srv://makesomaway:makesomaway@cluster0.movbe.mongodb.net/app?retryWrites=true&w=majority&appName=Cluster0";
+    
     if (!uri) {
       throw new Error("DATABASE_URL is missing in environment variables.");
     }
-  
+
     await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       ssl: true,
-      tlsAllowInvalidCertificates: false, 
-   
+      tlsAllowInvalidCertificates: false, // Keep this only if you have a cert validation requirement
     });
+
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1); // Exit with failure if connection fails
+    process.exit(1);
   }
 };
 
