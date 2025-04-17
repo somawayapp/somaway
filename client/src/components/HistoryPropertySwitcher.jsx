2d4bfed1fd93975e1c9a50f787e45b490fb9bd60
airnesygroup <airnesy.info@gmail.com>
2025-04-17 20:32:40 +0300

    Initial commit with new code

--- client/src/components/HistoryPropertySwitcher.jsx
+++ client/src/components/HistoryPropertySwitcher.jsx
@@ -59,31 +59,7 @@ export default function HistoryPropertySwitcher() {
         </div>
       )}
 
-      <div className="flex justify-between hidden md:flex gap-2 md:gap-4 items-center w-full max-w-sm">
-        {/* Clickable text */}
-        <div onClick={handleGoHome} className="cursor-pointer">
-          <p className="text-md text-[var(--softTextColor)] font-bold hover:underline">
-            Remove all filters
-          </p>
-          <p className="text-sm md:hidden block text-[var(--softTextColor)] font-normal">
-            go back home
-          </p>
-        </div>
-
-        {/* Fancy Toggle Switch */}
-        <div
-          onClick={handleToggle}
-          className="w-16 h-8 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-300 bg-[var(--softTextColor)]"
-        >
-          <div
-            className={`w-6 h-6 flex items-center justify-center text-[var(--softTextColor)] rounded-full shadow-md transform duration-300 ease-in-out ${
-              toggleState ? "translate-x-8 bg-[var(--bg)]" : "translate-x-0 bg-[var(--bg)]"
-            }`}
-          >
-            ✓
-          </div>
-        </div>
-      </div>
+     
 
       {isRootPathWithoutSearchParams && (
         <div className="flex justify-between  gap-0 md:gap-4 items-center w-full max-w-sm">
