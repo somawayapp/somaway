22ddea68d98ce293ac99264939d35cf73746333a
airnesygroup <airnesy.info@gmail.com>
2025-04-17 18:20:55 +0300

    Initial commit with new code

--- client/src/routes/HelpcenterPage.jsx
+++ client/src/routes/HelpcenterPage.jsx
@@ -18,7 +18,7 @@ const HelpCenter = () => {
           <div className="flex items-center space-x-3  mb-4">
             <h2 className="text-xl md:text-3xl font-semibold text-[var(--textColor)]">For Agents and Landlords</h2>
           </div>
-          <div className="space-y-4 md:space-y-8 ">
+          <div className="space-y-4 md:space-y-9 ">
             {/* Login Instructions */}
             <div className="rounded-xl shadow-lg border-[1px] border-[var(--softBg4)] p-4">
             <h3 className="flex items-center text-lg md:text-2xl space-x-2 font-semibold text-[var(--softTextColor)] mb-2">
@@ -117,11 +117,11 @@ const HelpCenter = () => {
         </section>
 
         {/* User (Tenant/Buyer) Help Section */}
-        <section className=" p-6 mb-8">
+        <section className=" p-6 mt-5 ">
         <div className="flex items-center space-x-3  mb-4">
         <h2 className="text-xl md:text-3xl font-semibold text-[var(--softTextColor)]">For Tenants and Buyers</h2>
           </div>
-          <div className="space-y-4 md:space-y-8 ">
+          <div className="space-y-4 md:space-y-9 ">
             {/* Viewing Listings */}
             <div className="rounded-xl shadow-lg border-[1px] border-[var(--softBg4)] p-4">
               <h3 className="font-semibold text-[var(--softTextColor)] mb-2">Viewing Listings</h3>
