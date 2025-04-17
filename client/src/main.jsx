e70b9c10ed43394692823d9f6b539675db5dd2b9
airnesygroup <airnesy.info@gmail.com>
2025-04-17 16:35:25 +0300

    Initial commit with new code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./routes/Homepage.jsx";
-import LandingPage from "./routes/LandingPage.jsx";
 import LoginPage from "./routes/LoginPage.jsx";
 import RegisterPage from "./routes/RegisterPage.jsx";
 import SinglePostPage from "./routes/SinglePostPage.jsx";
