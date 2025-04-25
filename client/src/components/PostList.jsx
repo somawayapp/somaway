e8c209cdc377f9896f4b2345eb232364c268e68a
airnesygroup <airnesy.info@gmail.com>
2025-04-25 08:40:20 +0300

    Initial commit with new code

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -160,13 +160,16 @@ const PostList = () => {
 
   if (displayedPosts.length === 0) {
     return (
-      <div style={{ display: "grid", gridTemplateColumns: columns}} className="gap-6 md:gap-9 scrollbar-hide">
-       {Array(8).fill(0).map((_, index) => (
-          <div style={{ height: 'calc(100vw / 8)' }} key={index} className="relative aspect-[3/3] w-full h-full">
-            <div className="absolute inset-0 bg-[var(--softBg4)] animate-pulse rounded-xl md:rounded-2xl"></div>
-          </div>
-        ))}
-      </div>
+      <div
+      style={{ display: "grid", gridTemplateColumns: columns }}
+      className="gap-6 md:gap-9 scrollbar-hide"
+    >
+      {Array(8).fill(0).map((_, index) => (
+        <div key={index} className="relative w-full h-0 pb-[100%]"> {/* This enforces aspect ratio */}
+          <div className="absolute inset-0 bg-[var(--softBg4)] animate-pulse rounded-xl md:rounded-2xl"></div>
+        </div>
+      ))}
+    </div>
     );
   }
 
