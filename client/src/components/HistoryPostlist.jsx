2e5925ab7b41166426ac9a67a32efecf65dfc4fd
airnesygroup <airnesy.info@gmail.com>
2025-04-25 07:50:47 +0300

    Initial commit with new code

--- client/src/components/HistoryPostlist.jsx
+++ client/src/components/HistoryPostlist.jsx
@@ -136,9 +136,9 @@ const HistoryPostList = () => {
     );
   }
 
-  if (displayedReviews.length === 0) {
+  if (displayedPosts.length === 0) {
     return (
-      <div style={{ display: "grid", gridTemplateColumns: columns }} className="gap-6 md:gap-9 scrollbar-hide">
+      <div style={{ display: "grid", gridTemplateColumns: columns,  height: 'calc(100vw * 16)' }} className="gap-6 md:gap-9 scrollbar-hide">
         {Array(8).fill(0).map((_, index) => (
           <div key={index} className="relative aspect-[3/1] w-full">
             <div className="absolute inset-0 bg-[var(--softBg4)] animate-pulse rounded-xl md:rounded-2xl"></div>
@@ -151,7 +151,7 @@ const HistoryPostList = () => {
   // Render the posts
   return (
      
-      <div style={{ display: "grid", gridTemplateColumns: columns,  height: 'calc(100vw * 16)' }} className="gap-6 md:gap-9 scrollbar-hide">
+      <div style={{ display: "grid", gridTemplateColumns: columns }} className="gap-6 md:gap-9 scrollbar-hide">
       {displayedPosts.map((post) => (
         <HistoryPostItem key={post._id} post={post} />
       ))}
