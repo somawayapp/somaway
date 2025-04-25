e5a1987135345e8bbf2a19758f14b50ed5c751c0
airnesygroup <airnesy.info@gmail.com>
2025-04-25 08:05:51 +0300

    Initial commit with new code

--- client/src/components/HistoryPostlist.jsx
+++ client/src/components/HistoryPostlist.jsx
@@ -136,16 +136,18 @@ const HistoryPostList = () => {
     );
   }
 
-  {displayedPosts.length === 0 && !showMessage && (
-    <div style={{ display: "grid", gridTemplateColumns: columns }} className="gap-6 h-full md:gap-9 scrollbar-hide">
-      {Array(8).fill(0).map((_, index) => (
-        <div key={index} className="relative w-full h-0 pb-[33.33%]">
-          <div className="absolute inset-0 bg-[var(--softBg4)] animate-pulse rounded-xl md:rounded-2xl"></div>
-        </div>
-      ))}
-    </div>
-  )}  
-  
+  if (displayedPosts.length === 0) {
+    return (
+      <div style={{ display: "grid", gridTemplateColumns: columns }} className="gap-6 md:gap-9 scrollbar-hide">
+        {Array(8).fill(0).map((_, index) => (
+          <div key={index} className="relative aspect-[3/1] w-full">
+            <div className="absolute inset-0 bg-[var(--softBg4)] animate-pulse rounded-xl md:rounded-2xl"></div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   // Render the posts
   return (
      
