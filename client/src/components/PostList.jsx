4ffbd55be6d5174a2c068a10f0bd676c21ea1e19
airnesygroup <airnesy.info@gmail.com>
2025-04-25 08:08:18 +0300

    Initial commit with new code

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -162,7 +162,7 @@ const PostList = () => {
     return (
       <div style={{ display: "grid", gridTemplateColumns: columns }} className="gap-6 md:gap-9 scrollbar-hide">
       {Array(8).fill(0).map((_, index) => (
-        <div style={{ height: 'calc(w * 3)' }} key={index} className="relative aspect-[3/3] w-full">
+        <div style={{ height: 'calc(w * 3)' }} key={index} className="relative aspect-[3/3]">
           <div className="absolute inset-0 bg-[var(--softBg4)] animate-pulse rounded-xl md:rounded-2xl"></div>
         </div>
       ))}
@@ -171,7 +171,7 @@ const PostList = () => {
   }
 
   return (
-    <div > style={{ display: "grid", gridTemplateColumns: columns }} className="gap-6 md:gap-9 scrollbar-hide">
+    <div  style={{ display: "grid", gridTemplateColumns: columns }} className="gap-6 md:gap-9 scrollbar-hide">
       {displayedPosts.map((post) => (
         <PostListItem key={post._id} post={post} />
       ))}
