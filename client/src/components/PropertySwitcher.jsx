f17bc8d5cdd77a9cdf5e32e701221867d62b6b16
airnesygroup <airnesy.info@gmail.com>
2025-05-14 18:08:59 +0300

    Initial commit with new code

--- client/src/components/PropertySwitcher.jsx
+++ client/src/components/PropertySwitcher.jsx
@@ -61,7 +61,7 @@ export default function PropertySwitcher() {
     <div className="w-full border p-3 border-[var(--softBg4)] rounded-xl mb-5 md:mb-3 shadow-md flex flex-col items-center relative">
       {/* Dropdown with Active Search Params */}
       {location.search && (
-        <div className="w-full md:min-w-[200px] mb-3">
+        <div className="w-full md:min-w-[225px] mb-3">
          <div className=" flex gap-6 mt-1 justify-between">
          <button
         className="text-sm ml-1 text-[var(--softTextColor)] transition-transform duration-200 hover:scale-105 underline font-semibold flex items-center gap-2"
@@ -78,7 +78,7 @@ export default function PropertySwitcher() {
          {showDropdown && (
   isMdOrLarger ? (
     // Modal
-    <div className="fixed inset min-w-[200px] z-50 flex">
+    <div className="fixed inset min-w-[225px] z-50 flex">
       <div className="mt-2 bg-[var(--bg)]  min-w-[200px] border border-[var(--softBg4)] rounded-md shadow-md p-3 text-sm space-y-2">
       {[...searchParams.entries()].map(([key, value]) => (
         <div key={key} className="flex justify-between items-center">
@@ -86,7 +86,7 @@ export default function PropertySwitcher() {
             {key}: <span className="font-semibold">{value}</span>
           </span>
           <button
-            className="text-blue-500 ml-1 text-xs hover:underline"
+            className="text-blue-500 ml-1 text-xs md:text-sm hover:underline"
             onClick={() => removeParam(key)}
           >
             Remove
@@ -95,7 +95,7 @@ export default function PropertySwitcher() {
       ))}
       <button
         onClick={clearAllParams}
-        className="mt-2 text-blue-600 text-xs font-bold hover:underline"
+        className="mt-2 text-blue-600 text-xs md:text-sm font-bold hover:underline"
       >
         Clear All Filters
       </button>
