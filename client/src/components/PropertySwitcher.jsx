603ea2f50f89fade56d57b0d4fd195a1d5d101b2
airnesygroup <airnesy.info@gmail.com>
2025-05-14 12:54:23 +0300

    Initial commit with new code

--- client/src/components/PropertySwitcher.jsx
+++ client/src/components/PropertySwitcher.jsx
@@ -99,7 +99,7 @@ export default function PropertySwitcher() {
  
 
 
-  <div className="flex items-center justify-between gap-3">
+  <div className="flex items-center justify-between ">
   <p
     onClick={() => handleClickFilter("")}
     className={`text-sm text-[var(--softTextColor)] hover:underline ${
