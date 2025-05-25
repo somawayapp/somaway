130aa5b5a2e6b241d154f49315a4754eb41a7ae0
airnesygroup <airnesy.info@gmail.com>
2025-05-25 12:36:29 +0300

    Initial commit with new code

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -78,7 +78,7 @@ from ${author || 'top authors'}in minutes. Elevate your mind—only on Hodi!`} /
   
 <div className="flex flex-col md:flex-row min-h-[75vh] relative">
   <Sidebar />
-<div className="flex-1  min-ml-[350px]">
+<div className="flex-1 md:ml-[21%]">
     <PostList />
            <Footer/>
 
