4bd1da0fa15cff7ea33e4c2c2dfdf10b806dfde6
airnesygroup <airnesy.info@gmail.com>
2025-05-25 11:08:06 +0300

    Initial commit with new code

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -88,7 +88,7 @@
               <img src="/betlogo2.png" className="h-[50px]" />
              </Link>
     
-             <div className="flex ml-[-5%] hidden md:flex items-center text-sm font-semibold justify-between flex-row gap-2 md:gap-9">
+             <div className="flex ml-[-15%] hidden md:flex items-center text-sm font-semibold justify-between flex-row gap-2 md:gap-9">
              <p className=" text-[#1ff8b0] hover:text-[#1ff8b0] transition cursor-pointer "> All sports</p>              
                <p className="text-[#f2f2f2] hover:text-[#1ff8b0] transition cursor-pointer"> In play</p>              
              <p className="text-[#f2f2f2] hover:text-[#1ff8b0] transition cursor-pointer" > Casinos</p>              
@@ -274,7 +274,7 @@
   
            </div>
 
-      <div className="grid grid-cols-3 md:hidden mb-2  gap-[10%] text-sm font-semibold">
+      <div className="grid grid-cols-3 md:hidden mb-2 px-6 text-sm font-semibold">
       <p className="text-left text-[#1ff8b0] hover:text-[#1ff8b0] transition cursor-pointer">All sports</p>
       <p className="text-center text-[#f2f2f2] hover:text-[#1ff8b0] transition cursor-pointer">In-play</p>
       <p className="text-right text-[#f2f2f2] hover:text-[#1ff8b0] transition cursor-pointer">Casinos</p>
