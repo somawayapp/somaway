74724bb1507908bb57c5a607c8d0bf8bd7b1d947
airnesygroup <airnesy.info@gmail.com>
2025-05-25 13:10:33 +0300

    Initial commit with new code

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -76,9 +76,10 @@ from ${author || 'top authors'}in minutes. Elevate your mind—only on Hodi!`} /
 
   
   
-<div className="flex flex-col md:flex-row min-h-[70vh]">
-  <Sidebar />
-  <div className="flex-1 ml-0 md:ml-[21%]  overflow-y-auto px-4 md:px-8">
+<div className="flex flex-col md:flex-row h-[70vh]">
+     <Sidebar />
+
+  <div className="flex-1 ml-0 md:ml-[20%] h-[70vh] overflow-y-auto px-4 md:px-8">
     <PostList />
     <Footer />
   </div>
