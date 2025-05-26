68685d4ca59e721a1911ea561b0e702ee5d51cd3
airnesygroup <airnesy.info@gmail.com>
2025-05-26 12:10:00 +0300

    Initial commit with new code

--- client/src/components/Carddata.jsx
+++ client/src/components/Carddata.jsx
@@ -3,40 +3,16 @@ import { Button } from "@mui/material";
 
 const cardData = [
   {
-    image: "/win1.jpg",
+    image: "/spin.webp",
     title: "Win Ksh 1 Million",
     desc: "Bet only 1 shilling and stand a chance to win 1 million shillings instantly.",
   },
-  {
-    image: "/win2.jpg",
-    title: "Win Ksh 100,000",
-    desc: "Turn 1 shilling into 100,000 shillings with one lucky bet.",
-  },
-  {
-    image: "/win3.jpg",
-    title: "Win Ksh 10,000",
-    desc: "One shilling could be your ticket to 10,000 shillings today.",
-  },
-  {
-    image: "/win4.jpg",
-    title: "Win Ksh 1,000",
-    desc: "Bet 1 shilling and you could walk away with 1,000 shillings.",
-  },
-  {
-    image: "/win5.jpg",
-    title: "Win Ksh 100",
-    desc: "Your single shilling could win you 100 shillings!",
-  },
-  {
-    image: "/win6.jpg",
-    title: "Win Ksh 10",
-    desc: "Even 1 shilling can win you 10 shillings. Start small, win big!",
-  },
+ 
 ];
 
 export default function BettingChances() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6  py-8  bg-[var(--bg)]">
+    <div className="grid grid-cols-1 gap-6  py-8  bg-[var(--bg)]">
       {cardData.map((item, i) => (
         <motion.div
           key={i}
@@ -49,7 +25,7 @@ export default function BettingChances() {
           <img
             src={item.image}
             alt={item.title}
-            className="w-full h-48 object-cover object-center hover:scale-105 transition-transform duration-300"
+            className="w-full  object-cover object-center hover:scale-105 transition-transform duration-300"
           />
           <div className="p-5 flex flex-col flex-grow">
             <h2 className="text-xl font-bold mb-2">{item.title}</h2>
