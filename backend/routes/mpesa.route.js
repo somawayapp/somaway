ce8c30a134823fa7f3518b2d09427a76f8c51272
airnesygroup <airnesy.info@gmail.com>
2025-07-01 19:46:53 +0300

    Initial commit with new code

--- backend/routes/mpesa.route.js
+++ backend/routes/mpesa.route.js
@@ -210,7 +210,7 @@ router.post("/stk-push", async (req, res) => {
 });
 
 // --- M-Pesa Callback Route ---
-router.post("/callback", async (req, res) => {
+router.post("/mpesa/callback", async (req, res) => {
   console.log("M-Pesa Callback received:", JSON.stringify(req.body, null, 2));
 
   const { Body } = req.body;
