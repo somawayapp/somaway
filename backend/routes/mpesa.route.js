bdfbcedf465c227d912a1e34fcda7246506869ce
airnesygroup <airnesy.info@gmail.com>
2025-07-16 18:08:03 +0300

    Initial commit with new code

--- backend/routes/mpesa.route.js
+++ backend/routes/mpesa.route.js
@@ -151,16 +151,24 @@ router.post("/stk-push", async (req, res) => {
       }
       // --- END ADDED DECRYPTED LOGGING ---
 
+      
+
       if (existingEntry.status === "Completed") {
         return res.status(409).json({
           success: false,
           error: "This phone number has already successfully participated in this cycle.",
         });
       } else if (existingEntry.status === "Pending") {
-        return res.status(409).json({
+        return res.status(200).json({
           success: true,
           error: "A transaction for this phone number is already pending. Please complete it or try again later.",
         });
+
+    
+
+
+
+        
       }
     } else {
         console.log("No existing entry found for this phone number in the current cycle.");
