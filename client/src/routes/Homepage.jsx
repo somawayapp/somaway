cc1aba14b6e74fca56ddab78e3201a13ad221499
airnesygroup <airnesy.info@gmail.com>
2025-08-12 17:07:10 +0300

    Initial commit

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -92,7 +92,7 @@ const HomePage = () => {
   <div className="flex md:flex-row relative"> 
 
     {/* Sidebar */}
-    <div className="w-full hidden sticky top-[110px] md:block md:w-[23%] h-[calc(100vh-130px)] overflow-y-auto border-r-2 border-[#1b1f1c]">
+    <div className="w-full hidden sticky top-[110px] md:block md:w-[18%] h-[calc(100vh-130px)] overflow-y-auto border-r-2 border-[#1b1f1c]">
       <Sidebar />
     </div>
 
